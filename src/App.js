fae188b568d325d6d7123e332d8ad4bbe8b793f6
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-12-11 08:15:12 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,1 +1,35 @@
-// create your App component here
+export default class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+          people: []
+        };
+      }
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json")
+          .then(response => response.json())
+          .then(data => {
+            console.log(data);
+            this.setState({
+              people: data.people
+            });
+          });
+      }
+      
+  
+    render() {
+      return (
+        <div>
+         <ul>
+            {this.state.people.map(person => {
+            return(
+                          
+                <li>{person.name}</li>
+                <li>{person.craft}</li>
+            )
+            }}
+         </ul>
+        </div>
+      );
+    }
+  }
