3c97c279cdd53905deac03ead8d8cc3b4c54e591
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-12-11 08:23:05 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-export default class App extends Component {
-  
-        state = {
-          people: []
-        };
-     
-    componentDidMount() {
-        fetch("http://api.open-notify.org/astros.json")
-          .then(response => response.json())
-          .then(data => {
-            console.log(data);
-            this.setState({
-              people: data.people
-            });
-          });
-      }
-      
-  
+// create your App component here
+import React, { Component } from 'react'
+class App extends Component {
+  state = {    peopleInSpace: []  }
+  render() {
+    return ( 
+      <div>  
+      {this.state.peopleInSpace.map(person => person.name)}      </div>    
+      )  
     
-  }
+  }   
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json') 
+    .then(response => response.json()) 
+    .then(data => {    
+      this.setState({       
+        peopleInSpace: data.people  
+        })   
+        })
+        }} 
+        export default App;
